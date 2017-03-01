f3cfb25e1558cbe2359aaa1c1d33bf263d8d73fb
mohankumar <mohankumar@mohan>
2017-03-01 12:38:25 +0530

    mohan sonar check-in

---
+++ SonarProject/src/com/mccoy/Hello.java
@@ -0,0 +1,14 @@
+package com.mccoy;
+
+public class Hello {
+	int a;
+	float b;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		
+
+	}
+
+}
